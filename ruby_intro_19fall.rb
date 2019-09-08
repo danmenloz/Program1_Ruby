41c12857c1d431973a87c85b25abe5fa008758ab
Daniel Mendez <dmendez@ncsu.edu>
2019-09-08 19:25:41 -0400

    Added code for unique_* functions

--- ruby_intro_19fall.rb
+++ ruby_intro_19fall.rb
@@ -10,13 +10,23 @@
   end
 
   def unique_total(a)
-    # ADD YOUR CODE HERE
+    #Create an array of repeated elements only
+    rep_array = a.select{ |elem| a.count(elem)>1 }
+    #Delete all repeated elements in input array 'a' and get the sum of the remaining elements
+    a.difference(rep_array).sum
   end
 
 # Part 2
 
   def unique_char(s)
-    # ADD YOUR CODE HERE
+    #Iterate through all characters in string 's' until one of them appears only once inside the string
+    s.each_char do |ch|
+      if (s.count ch) == 1
+        return s.index(ch) #Return the index of the unique char
+      end
+    end
+    #no unique char found! return default value
+    return -1
   end
 
   def reverse_words(s)
