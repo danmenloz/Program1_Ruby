0a1dbbcc12e9da2483057d341d77849df130619f
Daniel Mendez <dmendez@ncsu.edu>
2019-09-09 10:13:06 -0400

    merged Dylan and Daniel code

--- ruby_intro_19fall.rb
+++ ruby_intro_19fall.rb
@@ -2,14 +2,22 @@
   # Part 1
 
   def two_sum(a,k)
-    # ADD YOUR CODE HERE
+    # Dylan Spruill
+    return -1 if a.empty? || a.nil?
+    arr = a.combination(2).select {|x, y| x + y < k}
+    return -1 if arr.nil? || arr.empty?
+    return arr.max.sum
   end
 
   def sorted_squares(a)
-    # ADD YOUR CODE HERE
+    # Dylan Spruill
+    a.map! {|x| x ** 2}
+        .sort!
+    return a
   end
 
   def unique_total(a)
+    # Daniel Mendez
     #Create an array of repeated elements only
     rep_array = a.select{ |elem| a.count(elem)>1 }
     #Delete all repeated elements in input array 'a' and get the sum of the remaining elements
@@ -19,6 +27,7 @@
 # Part 2
 
   def unique_char(s)
+    # Daniel Mendez
     #Iterate through all characters in string 's' until one of them appears only once inside the string
     s.each_char do |ch|
       if (s.count ch) == 1
@@ -30,7 +39,14 @@
   end
 
   def reverse_words(s)
-    # ADD YOUR CODE HERE
+    # Dylan Spruill
+    words = s.downcase.split
+    i = 0
+    while i < words.length
+      words[i] = words[i].delete('aeiou')+words[i].scan(/[aeiou]/).join
+      i += 1
+    end
+    return words.reverse.join(" ")
   end
 
 # Part 3
